6f08af3cf69488d411712d8161b1ae9928a969dd
princeabrahams000 <princeabrahams000@gmail.com>
2024-08-02 19:59:40 +0530

    2Aug

--- mediaplayer/src/components/VideoCard.jsx
+++ mediaplayer/src/components/VideoCard.jsx
@@ -1,41 +1,55 @@
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
 
+function VideoCard(displayVideo) {
+  console.log(displayVideo);
 
+  const [show, setShow] = useState(false);
 
-function VideoCard() {
-    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <>
+      <Card style={{ width: "100%" }} className=" mt-3  ms-md-0">
+        <Card.Img
+          onClick={handleShow}
+          variant="top"
+          src={displayVideo?.image}
+          width={"100%"}
+          height={"300px"}
+        />
+        <Card.Body className="d-flex justify-content-between">
+          <Card.Text className="me-3">{displayVideo?.caption}</Card.Text>
+          
+          <Button variant="primary">
+            <FontAwesomeIcon icon={faTrashCan} />
+          </Button>
+        </Card.Body>
+      </Card>
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return (
-        <> 
-            <Card style={{ width: "100%" }} className=" mt-3  ms-md-0">
-                <Card.Img onClick={handleShow} variant="top" src="https://cdnb.artstation.com/p/assets/images/images/010/238/621/large/ananthu-s-kumar-poster-2.jpg?1523352277" width={'100%'} height={'300px'} />
-                <Card.Body className="d-flex justify-content-between">
-                   
-                    <Card.Text className="me-3">
-                       Movie Title
-                    </Card.Text>
-                    {/* <button className="btn btn-primary"><FontAwesomeIcon icon={faTrashCan} /></button> */}
-                    <Button variant="primary"><FontAwesomeIcon icon={faTrashCan} /></Button>
-                </Card.Body>
-            </Card>
-
-            <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Movie Title</Modal.Title>
+          <Modal.Title>{displayVideo?.caption}</Modal.Title>
         </Modal.Header>
-        <Modal.Body><iframe width="100%" height="444" src="https://www.youtube.com/embed/at9Jreair48?autoplay=1" title="Addiction (Official Video) Garry Sandhu | New Punjabi Video Song 2024 | Bipolar | Fresh Media Record" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></Modal.Body>
-       
+        <Modal.Body>
+          <iframe
+            width="100%"
+            height="444"
+            src={`${displayVideo?.url}? autoplay=1`}
+            title="Addiction (Official Video) Garry Sandhu | New Punjabi Video Song 2024 | Bipolar | Fresh Media Record"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+          ></iframe>
+        </Modal.Body>
       </Modal>
-
-        </>
-    );
+    </>
+  );
 }
 
 export default VideoCard;
--- mediaplayer/src/components/Vmus.jsx
+++ mediaplayer/src/components/Vmus.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import VideoCard from './VideoCard'
 import { getVideoApi } from '../services/allApi'
 
 
 function Vmus() {
+
+  const [videoDetails, setVideoDetails] = useState([])
+
   const getVideo = async()=>{
     const result = await getVideoApi()
-    console.log(result);
+    setVideoDetails(result.data)
   }
-  return (
-    <Row className='w-100 ms-1 '>
-      <Col xs={12} md={6} lg={4} xl={3} className='d-flex justify-content-center align-items-center'>
-        <VideoCard/>
-      </Col>
 
-    
-      
+  useEffect(()=>{
+    getVideo()
+  },[])
+  console.log(videoDetails);
+
+
+  return (
+
+    <Row className='w-100 ms-1 '>
+
+      {videoDetails?.length>0?
+        videoDetails?.map((item)=>( <Col xs={12} md={6} lg={4} xl={3} className='d-flex justify-content-center align-items-center'>
+          <VideoCard displayVideo ={item}/>
+        </Col>))
+        :
+        <p className='text-warning fs-5 mt-4'>No video yet uploaded....</p>}
     </Row>
   )
 }
